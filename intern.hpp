3908a5532c11d2763f34e9e1d7cea3c4e6214868
ekchew <ekchew@gmail.com>
2021-02-23 20:22:05 -0500

    reworking logic

--- intern.hpp
+++ intern.hpp
@@ -3,7 +3,6 @@
 
 #include <climits>
 #include <functional>
-#include <map>
 #include <memory>
 #include <mutex>
 #include <tuple>
@@ -12,108 +11,88 @@
 
 namespace intern {
 
-	enum class kMapType { Unordered, Ordered };
+	template<typename T, typename... Ts>
+		constexpr auto Hash(const T& v, const Ts&... vs) -> std::size_t {
+			if constexpr(sizeof...(Ts) == 0) {
+				return std::hash<T>{}(v);
+			}
+			else {
+				constexpr std::size_t kMagic =
+					sizeof(std::size_t) * CHAR_BIT > 32u ?
+					0x9e3779b97f4a7c15 : 0x9e3779b9;
+				auto h = Hash(vs...);
+				return h ^ (Hash(v) + kMagic + (h << 6) + (h >> 2));
+			}
+		}
 
 	namespace details {
-		template<typename Cls, kMapType MT> struct Interned;
-		template<typename Cls, kMapType MT>
-			struct Map {
-				using Type = std::unordered_map<
-					const Interned<Cls,kMapType::Unordered>*,
-					std::weak_ptr<const Interned<Cls,kMapType::Unordered>>
-					>;
-			};
-		template<typename Cls>
-			struct Map<Cls,kMapType::Ordered> {
-				using Type = std::map<
-					const Interned<Cls,kMapType::Ordered>*,
-					std::weak_ptr<const Interned<Cls,kMapType::Ordered>>
-					>;
-			};
-		template<typename Cls, kMapType MT>
-			using TMap = typename Map<Cls,MT>::Type;
-		template<typename Cls, kMapType MT>
-			struct Interned: Cls {
-				using TMutex = std::mutex;
-				template<typename... Args> static
-					auto MakeInterned(Args&&... args)
-						-> std::shared_ptr<const Interned>
-					{
-						using std::move;
-						std::shared_ptr<const Interned> p;
-						Interned v{std::forward<Args>(args)...};
-						std::lock_guard<TMutex> lg{gInternMapMutex};
-						auto it = gInternMap.find(&v);
-						if(it == gInternMap.end()) {
-							p = std::make_shared<const Interned>(move(v));
-							gInternMap.insert(it, std::make_pair(p.get(), p));
-						}
-						else {
-							p = it->second.lock();
-						}
-						return move(p);
-					}
-				using Cls::Cls;
-				~Interned() {
-						std::lock_guard<TMutex> lg{gInternMapMutex};
-						gInternMap.erase(this);
-					}
-			private:
-				inline static TMap<Cls,MT> gInternMap;
-				inline static TMutex gInternMapMutex;
-			};
-			template<typename Cls, kMapType MT>
-				auto operator == (
-					const Interned<Cls,MT>* pA,
-					const Interned<Cls,MT>* pB
-					)
-				{
-					return *pA == *pB;
-				}
-			template<typename Cls, kMapType MT>
-				auto operator < (
-					const Interned<Cls,MT>* pA,
-					const Interned<Cls,MT>* pB
-					)
-				{
-					return *pA < *pB;
-				}
+		struct TupleHash {
+			std::size_t mHash;
+			template<typename... Args>
+				constexpr TupleHash(const Args&... args):
+					mHash{Hash(args...)} {}
+		};
 	}
 
-	template<typename Cls, kMapType MT = kMapType::Unordered, typename... Args>
-		auto MakeInterned(Args&&... args)
-			-> std::shared_ptr<const details::Interned<Cls,MT>>
-		{
-			return details::Interned<Cls,MT>::MakeInterned(
-				std::forward<Args>(args)...
-				);
+	template<typename Tuple>
+		constexpr auto HashTuple(const Tuple& tuple) {
+			return std::make_from_tuple<details::TupleHash>(tuple).mHash;
 		}
 
-	template<typename T> constexpr
-		auto Hash(const T& v) -> std::size_t {
-			return std::hash<T>{}(v);
-		}
-	template<typename T, typename... Ts> constexpr
-		auto Hash(const T& v, const Ts&... vs) -> std::size_t {
-			constexpr std::size_t kMagic =
-				sizeof(std::size_t) * CHAR_BIT > 32u ?
-				0x9e3779b97f4a7c15 : 0x9e3779b9;
-			constexpr auto h = Hash(vs...);
-			return h ^ (Hash(v) + kMagic + (h << 6) + (h >> 2));
-		}
-	template<typename Tuple>
-		auto HashTuple(const Tuple& tuple) {
-			return std::apply(Hash, tuple);
+	constexpr bool kHasHashFn = true;
+	template<class Cls, bool HasHashFn>
+		class Interned: public Cls {
+			template<class Cls2, bool HasHashFn2, typename... Args>
+				friend auto MakeInterned(Args&&... args)
+					-> std::shared_ptr<const Interned<Cls2,HasHashFn2>>;
+			using TInternMap = std::unordered_map<
+				const Interned*, std::weak_ptr<const Interned>
+				>;
+			using TInternMut = std::mutex;
+			inline static TInternMap gInternMap;
+			inline static TInternMut gInternMut;
+		 public:
+			using Cls::Cls;
+			~Interned() {
+					std::lock_guard<TInternMut> lg{gInternMut};
+					gInternMap.erase(this);
+				}
+		};
+	template<class Cls, bool HasHashFn=false, typename... Args>
+		auto MakeInterned(Args&&... args)
+			-> std::shared_ptr<const Interned<Cls,HasHashFn>>
+		{
+			using TInterned = Interned<Cls,HasHashFn>;
+			using TMutex = typename TInterned::TInternMut;
+			std::shared_ptr<const TInterned> p;
+			TInterned val{std::forward<Args>(args)...};
+			std::lock_guard<TMutex> lg{TInterned::gInternMut};
+			auto& map = TInterned::gInternMap;
+			auto it = map.find(&val);
+			if(it == map.end()) {
+				p = std::make_shared<const TInterned>(std::move(val));
+				map.insert(it, std::make_pair(p.get(), p));
+			}
+			else {
+				p = it->second.lock();
+			}
+			return std::move(p);
 		}
 }
+
 namespace std {
-	template<typename Cls, intern::kMapType MT>
-		struct hash<const intern::details::Interned<Cls,MT>*> {
+	template<typename Cls, bool HasHashFn>
+		struct hash<const intern::Interned<Cls,HasHashFn>*> {
 			auto operator()(
-				const intern::details::Interned<Cls,MT>* pInterned)
+				const intern::Interned<Cls,HasHashFn>* p)
 					const noexcept -> std::size_t
 				{
-					return hash<Cls>{}(*pInterned);
+					if constexpr(HasHashFn) {
+						return p->hash();
+					}
+					else {
+						return hash<Cls>{}(*p);
+					}
 				}
 		};
 }
