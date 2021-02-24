335ba578743151412c0ac986932b526720e31288
ekchew <ekchew@gmail.com>
2021-02-23 21:41:42 -0500

    settled on a Tuple template arg idea that seems to work nicely

--- intern.hpp
+++ intern.hpp
@@ -11,12 +11,30 @@
 
 namespace intern {
 
+	//---- Hash Utilities ------------------------------------------------------
+	//
+	//	Hash(args...) -> std::size_t:
+	//		This function uses std::hash to calculate a hash value for each
+	//		arg you pass it. It requires at least one arg. If there are multiple
+	//		args, it combines the hash values into one before returning it.
+	//
+	//	HashTuple(arg) -> std::size_t:
+	//		This function unpacks a tuple and calls Hash() on its elements.
+	//		The "tuple" can really be anything tuple-like. In other words, arg
+	//		can be a std::pair or a std::array -- not just a std::tuple.
+	//
+
 	template<typename T, typename... Ts>
 		constexpr auto Hash(const T& v, const Ts&... vs) -> std::size_t {
 			if constexpr(sizeof...(Ts) == 0) {
 				return std::hash<T>{}(v);
 			}
 			else {
+				//	The algorithm used to combine hashes is based on the boost
+				//	algorithm. The 32-bit "magic" number has, however, been
+				//	extended to 64 bits where appropriate. Also note that the
+				//	combining is applied in reverse order since it was easier
+				//	to do so in a recursive function; not that it matters.
 				constexpr std::size_t kMagic =
 					sizeof(std::size_t) * CHAR_BIT > 32u ?
 					0x9e3779b97f4a7c15 : 0x9e3779b9;
@@ -33,18 +51,23 @@ namespace intern {
 					mHash{Hash(args...)} {}
 		};
 	}
-
 	template<typename Tuple>
 		constexpr auto HashTuple(const Tuple& tuple) {
 			return std::make_from_tuple<details::TupleHash>(tuple).mHash;
 		}
 
-	constexpr bool kHasHashFn = true;
-	template<class Cls, bool HasHashFn>
+	//---- Internment ----------------------------------------------------------
+	//
+	//	MakeInterned<Cls,Tuple>(args...)
+	//			-> std::shared_ptr<const Interned<Cls,Tuple>:
+	//		Given a hashable and equality-comparable class (Cls), this function
+	//		returns an interned shared pointer to an instance of it.
+
+	template<class Cls, typename Tuple>
 		class Interned: public Cls {
-			template<class Cls2, bool HasHashFn2, typename... Args>
+			template<class Cls2, typename Tuple2, typename... Args>
 				friend auto MakeInterned(Args&&... args)
-					-> std::shared_ptr<const Interned<Cls2,HasHashFn2>>;
+					-> std::shared_ptr<const Interned<Cls2,Tuple2>>;
 			using TInternMap = std::unordered_map<
 				const Interned*, std::weak_ptr<const Interned>
 				>;
@@ -58,11 +81,11 @@ namespace intern {
 					gInternMap.erase(this);
 				}
 		};
-	template<class Cls, bool HasHashFn=false, typename... Args>
+	template<class Cls, typename Tuple=void, typename... Args>
 		auto MakeInterned(Args&&... args)
-			-> std::shared_ptr<const Interned<Cls,HasHashFn>>
+			-> std::shared_ptr<const Interned<Cls,Tuple>>
 		{
-			using TInterned = Interned<Cls,HasHashFn>;
+			using TInterned = Interned<Cls,Tuple>;
 			using TMutex = typename TInterned::TInternMut;
 			std::shared_ptr<const TInterned> p;
 			TInterned val{std::forward<Args>(args)...};
@@ -81,18 +104,22 @@ namespace intern {
 }
 
 namespace std {
-	template<typename Cls, bool HasHashFn>
-		struct hash<const intern::Interned<Cls,HasHashFn>*> {
+	template<typename Cls, typename Tuple>
+		struct hash<const intern::Interned<Cls,Tuple>*> {
 			auto operator()(
-				const intern::Interned<Cls,HasHashFn>* p)
+				const intern::Interned<Cls,Tuple>* p)
 					const noexcept -> std::size_t
 				{
-					if constexpr(HasHashFn) {
-						return p->hash();
-					}
-					else {
-						return hash<Cls>{}(*p);
-					}
+					return intern::HashTuple(static_cast<Tuple>(*p));
+				}
+		};
+	template<typename Cls>
+		struct hash<const intern::Interned<Cls,void>*> {
+			auto operator()(
+				const intern::Interned<Cls,void>* p)
+					const noexcept -> std::size_t
+				{
+					return hash<Cls>{}(*p);
 				}
 		};
 }
